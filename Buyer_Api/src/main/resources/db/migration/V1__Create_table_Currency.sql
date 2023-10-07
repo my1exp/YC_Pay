837d595b99c79b9551124bcb2c1a87b4832d6b2b
Nikita Zhuravets <impozernovus@gmail.com>
2023-10-07 20:28:41 +0300

    Delete Buyer_Api directory

--- Buyer_Api/src/main/resources/db/migration/V1__Create_table_Currency.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE if not exists Currency (
-                            id SERIAL PRIMARY KEY,
-                            name VARCHAR(30),
-                            network VARCHAR(30)
-);

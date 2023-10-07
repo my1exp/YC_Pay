6c67e459515d7e9e1048a86f332a3ce658aaf123
Nikita Zhuravets <impozernovus@gmail.com>
2023-10-07 20:26:12 +0300

    add CryptoManager Service

---
+++ CryptoManager/src/main/java/com/yc_pay/controller/WalletController.java
@@ -0,0 +1,25 @@
+package com.yc_pay.controller;
+
+import com.yc_pay.model.WalletResponse;
+import com.yc_pay.service.WalletService;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Header;
+
+@Controller()
+public class WalletController {
+
+    private final WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @Get("/wallet")
+    public HttpResponse<WalletResponse> getWalletToBuyer(@Header String network,
+                                                         @Header String currency)
+    {
+        return HttpResponse.ok(walletService.getWalletToBuyer(network, currency));
+    }
+}

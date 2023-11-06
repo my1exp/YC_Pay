3ec4fd697df85f7125530de97e5d0f83ce39fcbd
Nikita Zhuravets <impozernovus@gmail.com>
2023-11-06 17:05:03 +0300

    add app.properties

---
+++ Pricer/src/main/java/com/yc_pay/Api/CmcApi.java
@@ -0,0 +1,48 @@
+package com.yc_pay.Api;
+
+import jakarta.inject.Singleton;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Singleton
+public class CmcApi {
+    private static String apiKey = "7e11e445-d09a-474d-86c4-bdfd20f3b094";
+
+    public static String makeAPICall(String uri, List<NameValuePair> parameters)
+            throws URISyntaxException, IOException {
+        String response_content = "";
+
+        URIBuilder query = new URIBuilder(uri);
+        query.addParameters(parameters);
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet request = new HttpGet(query.build());
+
+        request.setHeader(HttpHeaders.ACCEPT, "application/json");
+        request.addHeader("X-CMC_PRO_API_KEY", apiKey);
+
+        CloseableHttpResponse response = client.execute(request);
+
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            response_content = EntityUtils.toString(entity);
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+        return response_content;
+    }
+}
